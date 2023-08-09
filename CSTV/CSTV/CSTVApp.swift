4deedc134a49b9b23fd37f05301602e77f797e70
Caio Berkley <caioberkley@gmail.com>
2023-08-09 13:08:30 -0300

    Splash screen added

---
+++ CSTV/CSTV/CSTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSTVApp.swift
+//  CSTV
+//
+//  Created by Caio Berkley on 09/08/23.
+//
+
+import SwiftUI
+
+@main
+struct CSTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
